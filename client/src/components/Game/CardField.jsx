8117025c66ddc72b564db5ebef8cd22e1b57fd03
Scream199109 <Scream199109@mail.ru>
2022-05-20 11:10:29 +0300

    pravki

---
+++ client/src/components/Game/CardField.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CardField({ price }) {
+  return (
+    <div className="field__card">{price}</div>
+  );
+}
+
+export default CardField;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,15 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home(props) {
+
   return (
     <div>
-      <h1>Home</h1>
-    </div>
+      <Button>
+        <Link to='/game'>Играть</Link>
+      </Button>
+    </div >
   );
 }
 
